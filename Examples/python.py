f70ea89d96f968de621dd1e1e175da27fc27b4ff
mariolatiffathy <32080032+mariolatiffathy@users.noreply.github.com>
2018-11-22 15:29:54 +0200

    Update python.py

--- Examples/python.py
+++ Examples/python.py
@@ -1,11 +1,11 @@
 import requests
 r_GetCookie = requests.get("http://test.epizy.com")
-r_GetCookie_Response = r_GetCookie.text
+r_GetCookie_Response = r_GetCookie.text # This is the JavaScript code.
 
 r_GetCookie_2 = requests.post("https://edgy.faked.to/getbyethostcookie.php", data={"jscode":r_GetCookie_Response})
 r_GetCookie_2_Response = r_GetCookie_2.text # This is the cookie! (__test=X, where X is the cookie.)
 
-test_cookie = r_GetCookie_2_Response.split("=")[1]
+test_cookie = r_GetCookie_2_Response.split("=")[1] # Get the text after `=` sign (which is the __test cookie value)
 
 r = requests.get("http://test.epizy.com", cookies={"__test":test_cookie})
 r_Response = r.text
