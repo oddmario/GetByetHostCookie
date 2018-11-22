49d831c036b0d23ddd0c0d3bafc26c804ca91299
mariolatiffathy <32080032+mariolatiffathy@users.noreply.github.com>
2018-11-22 15:40:00 +0200

    Create php.php

---
+++ Examples/php.php
@@ -0,0 +1,31 @@
+<?php
+$r_GetCookie = file_get_contents("http://test.epizy.com"); // This is the JavaScript code.
+
+$postdata = http_build_query(
+    array(
+        'jscode' => $r_GetCookie
+    )
+);
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $postdata
+    )
+);
+$context  = stream_context_create($opts);
+
+$r_GetCookie_2 = file_get_contents('https://edgy.faked.to/getbyethostcookie.php', false, $context); // This is the cookie! (__test=X, where X is the cookie.)
+$__test_cookie = substr($r_GetCookie_2, strpos($r_GetCookie_2, "=") + 1); // Get the text after `=` sign (which is the __test cookie value)
+
+$opts2 = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Cookie: __test=" . $__test_cookie
+  )
+);
+$context2 = stream_context_create($opts2);
+
+$r = file_get_contents("http://test.epizy.com", false, $context2);
+echo $r;
+?>
